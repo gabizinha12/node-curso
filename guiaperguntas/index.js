1931dbd7afba93d7a038aa9607fe4afec830e5e9
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-09-04 18:55:29 -0300

    iniciando projeto

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -3,20 +3,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/:nome/:lang", function (req,res){
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-    var produtos = [{nome: "Doritos", preco: 3.14},
-                    {nome: "Coca-Cola", preco: 5.00},
-                    {nome: "Leite", preco: 1.45}
-]   
-    res.render('index',{
-       nome: nome,
-       lang: lang,
-       msg: exibirMsg,
-       produtos: produtos
-   });
+app.get("/", function (req,res){
+
 });
 
 app.listen(8080, () => {
