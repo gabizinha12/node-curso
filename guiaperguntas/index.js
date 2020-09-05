62ceed25d000e7f13999094f7f5561a03746f067
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-09-04 22:24:14 -0300

    formulario sendo recebido no back-end

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", function (req,res){
+   res.render("index");
+});
 
+
+app.get("/perguntar", function(req,res) {
+    res.render("perguntar");
+});
+
+
+app.post("/salvarpergunta", function(req,res) {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido  "  + titulo +  " "   + descricao);
 });
 
 app.listen(8080, () => {
