47d16b611c0c1e9055cefc46bf3dc90e22b49a5f
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-09-07 02:39:48 -0300

    subindo models e views

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
 const app = express();
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); // View Enginw
 
+
+// Database
 connection.authenticate().then(() => {
     console.log("Conexão feita com o banco de dados");
 }).catch((erro) => {
@@ -17,7 +21,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/", function (req,res){
-   res.render("index");
+  Pergunta.findAll({raw:true, order: [
+      ['id', 'DESC']
+  ]}).then((perguntas) => {
+    res.render("index", {
+        perguntas: perguntas
+    });
+})
 });
 
 
@@ -26,10 +36,43 @@ app.get("/perguntar", function(req,res) {
 });
 
 
+app.get("/pergunta/:id", function(req, res) {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+     if(pergunta != undefined) {
+     res.render("pergunta", {
+         pergunta: pergunta
+     });
+     }else {
+       res.redirect("/");
+     }
+    });
+});
+
+
 app.post("/salvarpergunta", function(req,res) {
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
-    res.send("Formulário recebido  "  + titulo +  " "   + descricao);
+   Pergunta.create({
+       titulo: titulo,
+       descricao: descricao
+   }).then(()=> {
+      res.redirect("/");
+   });
+});
+
+
+app.post("/responder", function(req, res) {
+   var corpo = req.body.corpo;
+   var perguntaId = req.body.pergunta;
+   Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId
+   }).then(() => {
+      res.redirect("/pergunta"+perguntaId)
+   });
 });
 
 app.listen(8080, () => {
