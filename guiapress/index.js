1c05b90eb083bc9bb0c2de26f4e64b0952f48c86
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-10-17 00:12:25 -0300

    paginação arrumada

--- guiapress/index.js
+++ guiapress/index.js
@@ -34,8 +34,12 @@ app.use("/", articlesController)
 
 
 app.get("/", (req,res) => {
-    Article.findAll().then((articles) => {
-
+    Article.findAll({
+        order: [
+            ['id', 'DESC']
+        ],
+        limit: 4
+    }).then((articles) => {
       Category.findAll().then((categories) => {
         res.render("index", {articles: articles, categories: categories})
 
